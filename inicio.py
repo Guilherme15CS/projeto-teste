48891934315bb4467e8cd14288cc08853f24a90d
Guilherme <guicseleg@gmail.com>
2024-08-20 21:23:43 -0300

    começo das condições

--- inicio.py
+++ inicio.py
@@ -1,3 +1,8 @@
 nome = input('Digite seu nome: ')
 idade = int(input('Quantos anos você tem? '))
 print(f'Seu nome é {nome} e tem {idade} anos')
+if idade >= 18:
+    print('Você é maior de idade, você tem liberdade para a vida')
+else:
+    resto = 18 - idade
+    print(f'Você ainda é menor de idade, continue estudando para que em {resto} anos você seja um adulto independente')
