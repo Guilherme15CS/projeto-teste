c88ec953606bc6bb4234cf556c4a78aee7b66006
Guilherme <guicseleg@gmail.com>
2024-08-20 21:16:23 -0300

    verificação da idade

--- inicio.py
+++ inicio.py
@@ -1,3 +1,11 @@
 nome = input('Digite seu nome: ')
 idade = int(input('Quantos anos você tem? '))
 print(f'Seu nome é {nome} e tem {idade} anos')
+if idade <= 10:
+    print('Você ainda está no fundamental 1')
+elif 11 <= idade <= 14:
+    print('Você está no fundamental 2')
+elif 15 <= idade <=18:
+    print('Você está no ensino médio')
+else:
+    print('Você já terminou a escola')
